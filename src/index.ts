8e8d6dfe5b74888678fef9d180abfd5e9b5425e5
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-01-30 22:36:58 +0100

    add secrete keys

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ new VoltAgent({
   logger,
   observability,
   voltOpsClient: new VoltOpsClient({
-    publicKey: process.env.VOLTAGENT_PUBLIC_KEY || "",
-    secretKey: process.env.VOLTAGENT_SECRET_KEY || "",
+    publicKey: process.env.VOLTAGENT_PUBLIC_KEY || "mysecretkey",
+    secretKey: process.env.VOLTAGENT_SECRET_KEY || "mysecretekey",
   }),
 });
